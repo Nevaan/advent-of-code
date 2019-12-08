afaa55c3b2622ea01b536432042eccd92d2bcc54
Nevaan <pawel.losek@outlook.com>
2019-12-08 20:30:37 +0100

    Day 8 task 1 solution

--- 2019/src/8/__tests__/task15.test.ts
+++ 2019/src/8/__tests__/task15.test.ts
@@ -1,8 +1,31 @@
+import { countLayersNumber, sliceIntoLayers, mapLayerIntoNumberArray, findLayerWithFewestDigit } from '../task15';
+
 describe('task15', () => {
 
 
-    test('', () => {
-        expect(true).toBe(true);
+    test('countLayersNumber', () => {
+
+        expect(countLayersNumber("123456789012", 2, 3)).toBe(2);
     })
+
+    test('sliceIntoLayers', () => {
+        expect(sliceIntoLayers("123456789012", 2, 3)).toEqual([
+            "123456",
+            "789012"
+        ])
+    })
+
+    test('mapLayerIntoNumberArray', () => {
+        expect(mapLayerIntoNumberArray("1234567890")).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+    })
+
+    test('findLayerWithFewestDigit', () => {
+        
+        expect(findLayerWithFewestDigit("123456789012005436324154",2 ,3, 0)).toEqual(
+            [
+                0,0,5,4,3,6
+            ]
+        )
+    });
 
 })
--- 2019/src/8/task15.ts
+++ 2019/src/8/task15.ts
@@ -1,2 +1,47 @@
+import { day8Input } from './input8';
+
+export const countLayersNumber = (input: string, h: number, w: number): number => {
+    return input.length / (w*h);
+}
+
+export const sliceIntoLayers = (input: string, h: number, w: number): string[] => {
+
+    let layers: string[]= [];
+    let chunkSize = h*w;
+    let idx = 0;
+
+    while(chunkSize <= input.length) {
+        layers = [...layers, input.slice(idx, chunkSize)];
+        idx += h*w;
+        chunkSize += h*w;
+    }
+
+    return layers;
+}
+
+export const mapLayerIntoNumberArray = (input: string): number[] => {
+    return input.split("").map(e => +e);
+}
+
+export const findLayerWithFewestDigit = (input: string, h: number, w: number, digit: number): number[] => {
+    let layers = sliceIntoLayers(input, h, w)
+        .map(layer => mapLayerIntoNumberArray(layer));
+
+
+
+    let fewestDigitsLayerIdx = layers
+        .map((layer, idx) => { return { digits: countDigit(layer, digit), idx: idx}})
+        .sort((digitsCount1, digitsCount2) => digitsCount1.digits - digitsCount2.digits)
+    return layers[fewestDigitsLayerIdx[0].idx];
+} 
+
+const countDigit = (input: number[], digit: number): number => {
+    return input.filter(a => a===digit).length;
+}
+
+export const task15Fn = () => {
+    let fewest0Layer = findLayerWithFewestDigit(day8Input, 6, 25, 0);
+    return countDigit(fewest0Layer, 1) * countDigit(fewest0Layer, 2); 
+}
 
 require('make-runnable');
