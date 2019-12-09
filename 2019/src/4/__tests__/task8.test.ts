a249747cb4c1beb073d00002a1db151aa0dc5ab7
Nevaan <pawel.losek@outlook.com>
2019-12-09 20:49:53 +0100

    Day 4 task 8

--- 2019/src/4/__tests__/task8.test.ts
+++ 2019/src/4/__tests__/task8.test.ts
@@ -1,8 +1,13 @@
+import { checkCriteria } from "../task7";
+import { task8Criteria } from "../task8";
+
 describe('task8', () => {
 
 
     test('', () => {
-        expect(true).toBe(true);
+        expect(checkCriteria(112233, task8Criteria)).toBe(true);
+        expect(checkCriteria(123444, task8Criteria)).toBe(false);
+        expect(checkCriteria(111122, task8Criteria)).toBe(true);
     })
 
 })
--- 2019/src/4/task8.ts
+++ 2019/src/4/task8.ts
@@ -1,2 +1,35 @@
+import { day4LowerLimit, day4UpperLimit } from "./input4";
+import { checkCriteria, task7TestFns } from "./task7";
+
+export const task8Criteria = [
+    ...task7TestFns,
+    (x: number): boolean => {
+        let xAsString = "" + x;
+        let digitArray: string[] = xAsString.split("");
+        let occurenceMap: Map<string, number> = new Map<string, number>();
+        digitArray.forEach(elem => {
+            let occurences = occurenceMap.get(elem);
+            if(occurences === undefined) {
+                occurenceMap.set(elem, 1);
+            } else {
+                occurenceMap.set(elem, occurences + 1);
+            }
+        });
+        let counts: number[] = [];
+         for( let entry of occurenceMap.values()) {
+            counts = [...counts, entry];
+         }
+        return counts.some(x => x===2);
+    }
+]
+
+export const task8Fn = () => {
+    let count = 0;
+    for(let i = day4LowerLimit; i <= day4UpperLimit; i++) {
+        if(checkCriteria(i, task8Criteria)) count ++;
+    }
+
+    return count;
+}
 
 require('make-runnable');
